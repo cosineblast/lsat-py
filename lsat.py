daf2514921edf6a9d16c8a88856735f4ecf32aa0
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-06-26 22:47:37 -0300

    Move inequations into separate module

--- lsat.py
+++ lsat.py
@@ -1,7 +1,6 @@
-# TODO: consider optimizing out s and d unecessary variables
-# in FormulaBuilder
-
 from rich import print
+
+import equations
 
 def minus(a, b):
     return ("minus", a, b)
@@ -15,6 +14,20 @@ class FormulaBuilder:
     _var_counter = 0
 
     def add_formula(self, input_formula):
+        def add_binary_formula(left, right, fn):
+            a = self.add_formula(left)
+            b = self.add_formula(right)
+
+            y = self._add_variable()
+            beta = self._add_variable()
+
+            equations = fn(a,b,beta,y)
+
+            for equation in equations:
+                self._add_equation(equation)
+
+            return y
+
         match input_formula:
             case ("Negation", formula):
                 t = self.add_formula(formula)
@@ -33,232 +46,20 @@ class FormulaBuilder:
             case ('Atomic', name):
                 return self._add_variable("y_" + name)
 
-            case ("Disjunction", left_formula, right_formula):
-                a = self.add_formula(left_formula)
-                b = self.add_formula(right_formula)
+            case ("Disjunction", left, right):
+                return add_binary_formula(left, right, equations.disjunction_equations)
 
-                y = self._add_variable()
-                beta = self._add_variable()
+            case ("Conjunction", left, right):
+                return add_binary_formula(left, right, equations.conjunction_equations)
 
-                """
-                `s - β ≤ y ≤ s`
+            case ("Max", left, right):
+                return add_binary_formula(left, right, equations.max_equations)
 
-                s - β ≤ y →
-                s - β - y ≤ 0 →
-                a + b - β - y ≤ 0 →
-                [a, b, β, y] ∙ [1, 1, -1, -1] ≤ 0
-
-                y ≤ s →
-                y ≤ a + b →
-                y - a - b ≤ 0
-                [y, a, b] ∙ [1, -1, -1] ≤ 0
-                -}
-
-                {-
-                `β ≤ y ≤ 1`
-
-                β ≤ y →
-                β - y ≤  0 →
-                [β, y] ∙ [1,-1] ≤ 0
-
-                y ≤ 1 →
-                [y] ∙ [1] ≤ 1
-                """
-
-                equations = [
-                    ('le', [a,b,beta,y], [1,1,-1,-1], 0),
-                    ('le', [y,a,b], [1,-1,-1], 0),
-                    ('le', [beta, y], [1, -1], 0),
-                    ('le', [y], [1], 1)
-                ]
-
-                for equation in equations:
-                    self._add_equation(equation)
-
-                return y
-
-            case ("Conjunction", left_formula, right_formula):
-                a = self.add_formula(left_formula)
-                b = self.add_formula(right_formula)
-
-                y = self._add_variable()
-                beta = self._add_variable()
-
-                """
-                Conjunction
-
-                1. `d ≤ y ≤ d + β`
-                d ≤ y →
-                d - y ≤ 0 →
-                a+b-1-y ≤ 0
-                a + b - y ≤ 1
-                [a, b, y] ∙ [1, 1, -1] ≤ 1
-
-                y ≤ d + β →
-                y - d - β ≤ 0
-                y - (a + b - 1) - β ≤ 0
-                y - a - b + 1 - β ≤ 0
-                y - a - b - β ≤ -1
-                [y, a, b, β] ∙ [1, -1, -1, -1] ≤ -1
-
-                2. `0 ≤ y ≤ 1 - β`
-
-                0 ≤ y →
-                - y ≤ 0 →
-                [y] ∙ [-1] ≤  0
-
-                y ≤ 1 - β →
-                y + β ≤ 1
-                [y,β] ∙ [1,1] ≤ 1
-                """
-
-                equations = [
-                    ('le',[a,b,y], [1,1,-1],  1),
-                    ('le',[y,a,b,beta], [1,-1,-1,-1],  -1),
-                    ('le',[y], [-1],  0),
-                    ('le',[y, beta], [1,1],  1)
-                ]
-
-                for equation in equations:
-                    self._add_equation(equation)
-
-                return y
-
-            case ("Max", left_formula, right_formula):
-                a = self.add_formula(left_formula)
-                b = self.add_formula(right_formula)
-
-                y = self._add_variable()
-                beta = self._add_variable()
-
-                """
-                1. `a ≤ y ≤ a + β`
-                a ≤ y →
-                a - y ≤ 0 →
-                [a, y] ∙ [1, -1] ≤ 0
-
-                y ≤ a + β
-                y - a - β ≤ 0
-                [y,a,β] ∙ [1,-1,-1] ≤ 0
-
-
-                2. `b ≤ y ≤ b + (1 - β)`
-
-                b ≤ y
-                b - y ≤ 0
-                [b, y] ∙ [1,-1] ≤ 0
-
-                y ≤ b + (1 - β)
-                y - b + β ≤ 1
-                [y,b,β] ∙ [1,-1,1] ≤ 1
-                """
-
-                equations = [
-                    ('le',[a,y], [1,-1],  0),
-                    ('le',[y,a,beta], [1,-1,-1],  0),
-                    ('le',[b,y], [1,-1],  0),
-                    ('le',[y,b,beta], [1,-1,1],  1)
-                ]
-
-                for equation in equations:
-                    self._add_equation(equation)
-
-                return y
-
-            case ("Min", left_formula, right_formula):
-                a = self.add_formula(left_formula)
-                b = self.add_formula(right_formula)
-
-                y = self._add_variable()
-                beta = self._add_variable()
-
-                """
-                Min
-
-                1. `a - β ≤ y ≤ a`
-                a - β ≤ y
-                a - β - y ≤ 0
-                [a, β, y] ∙ [1, -1, -1] ≤ 0
-
-                y ≤ a
-                y - a ≤ 0
-                [y,a] ∙ [1,-1] ≤ 0
-
-
-                2. `b - (1 - β) ≤ y ≤ b`
-                b - (1 - β) ≤ y
-                b - (1 - β) - y ≤ 0
-                b - 1 + β - y ≤ 0
-                b + β - y ≤ 1
-
-                [b, β, y] ∙ [1,1,-1] ≤ 1
-
-                y ≤ b
-                y - b ≤ 0
-                [y,b] ∙ [1, -1] ≤ 0
-                """
-
-
-                equations = [
-                    ('le',[a, beta, y], [1, -1, -1],  0),
-                    ('le',[y, a], [1, -1],  0),
-                    ('le',[b, beta, y], [1, 1, -1],  1),
-                    ('le',[y, b], [1, -1],  0)
-                ]
-
-                for equation in equations:
-                    self._add_equation(equation)
-
-                return y
+            case ("Min", left, right):
+                return add_binary_formula(left, right, equations.min_equations)
 
             case ('Implication', left, right):
-                a = self.add_formula(left)
-                b = self.add_formula(right)
-
-                y = self._add_variable()
-                beta = self._add_variable()
-
-                """
-                Implication
-
-                1. s - β ≤ y ≤ s
-                s = 1 + b - a
-
-                s - β ≤ y
-                1 + b - a - β ≤ y
-                1 + b - a - β - y ≤ 0
-                b - a - β - y ≤ -1
-
-                [b,a,β,y] ∙ [1,-1,-1,-1] ≤ -1
-
-                y ≤ s
-                y ≤ 1 + b - a
-                y - b + a ≤ 1
-                [y,b,a] ∙ [1,-1,1] ≤ 1
-
-                2. β ≤ y ≤ 1
-
-                β ≤ y
-                β - y ≤ 0
-                [β, y] ∙ [1, -1] ≤ 0
-
-                y ≤ 1
-                [y] ∙ [1] ≤ 1
-                """
-
-                equations = [
-                    ('le',[b,a,beta,y], [1,-1,-1,-1],  -1),
-                    ('le',[y,b,a], [1,-1,1],  1),
-                    ('le',[beta,y], [1,-1],  0),
-                    ('le',[y], [1],  1)
-                ]
-
-                for equation in equations:
-                    self._add_equation(equation)
-
-                return y
-
-
+                return add_binary_formula(left, right, equations.implication_equations)
 
         raise ValueError('Unknown formula: {}'.format(input_formula))
 
@@ -279,6 +80,8 @@ class FormulaBuilder:
             return "x_{}".format(current)
         else:
             return name
+
+
 
 
 def build_equations(assignments):
